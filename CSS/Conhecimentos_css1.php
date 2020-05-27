a6f442d340116a4dac29ecce08054b2cb2e28d18
samuelphp <57539966+samuelphp@users.noreply.github.com>
2020-05-26 21:47:24 -0300

    Add files via upload

---
+++ CSS/Conhecimentos_css1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="conhecimentoss.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+
+    <style type="text/css">
+    	#menu {
+    		background-color: gray;
+    	}
+    	
+    	#conteudo_principal {
+    		background-color: blue;
+    	}
+
+    	#rodape {
+    		background-color: gray;
+    	}
+    	.verde {
+    		color: green;
+    	}		
+    </style>
+</head>
+	<body>
+		<div id="divcurso">
+			<h3>Conhecimentos1 - Span e Div<h3>
+			<div id="menu">
+				Home | Quem Somos | Contato		
+			</div>
+			<div id="conteudo_principal">
+			<h2>Conteúdo Principal</h2>
+			<p>Conteúdo</p>			
+			</div>
+			<div id="rodape">
+				Todos os <span class="verde">direitos</span> reservados
+			</div>
+		</div>
+	</body>
+</html>
+ 
---
+++ CSS/Conhecimentos_css2 php.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="conhecimentoss.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+
+    <style type="text/css">
+       .conteudo {
+       	font-family: "Trebuchet MS", Helvetica, sans-serif;
+       	font-size: 19px;
+       	border-color: yellow yellow blue blue;
+       	border-width: 10px 10px;
+       	border-style: solid;
+       	margin-right: 15px; 
+       	margin-left: 15px; 
+       
+		
+     }
+
+     #conteudo2 {
+     	font-family: "Trebuchet MS", Helvetica,sans-serif;
+     	font-size: 19px;
+     	border-color: red red green green;
+     	border-width: 10px 10px;
+     	border-style: solid; 
+     }
+    </style>
+</head>
+	<body>
+		<div id="divcurso">
+			<span class="conteudo">
+				Conteúdo
+			</span>
+			<span id="conteudo2">
+				Conteúdo2
+			</span>
+			<span class="conteudo">
+				Conteúdo 3
+			</span>
+
+		</div>
+	</body>
+</html>
+ 
